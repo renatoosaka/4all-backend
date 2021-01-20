1f09cc3d24beb5921f0d1b4472aaf53bc2612757
Renato Hideki Osaka <renato.osaka@hotmail.com>
2021-01-20 16:21:10 -0300

    chore: register dependencies injection

---
+++ src/shared/container/index.ts
@@ -0,0 +1,11 @@
+import { container } from 'tsyringe';
+
+import '@modules/customers/providers';
+
+import CustomersRepository from '@modules/customers/infra/typeorm/repositories/CustomersRepository';
+import ICustomersRepository from '@modules/customers/repositories/ICustomersRepository';
+
+container.registerSingleton<ICustomersRepository>(
+  'CustomersRepository',
+  CustomersRepository,
+);
