e37d215cc64ce0ae0b7a7746e470e935caccb9f9
Renato Hideki Osaka <renato.osaka@hotmail.com>
2021-01-20 15:28:39 -0300

    chore: configure some middlewares on express

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 
 import express from 'express';
+import cors from 'cors';
 
 import routes from './routes';
 
@@ -8,6 +9,9 @@ import '@shared/infra/typeorm';
 
 const app = express();
 
+app.disable('x-powered-by');
+app.use(cors());
+app.use(express.json());
 app.use(routes);
 
 const PORT = process.env.PORT || 5000;
