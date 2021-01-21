d85b93d31b129e66532f775af4d06aa1f236ca32
Renato Hideki Osaka <renato.osaka@hotmail.com>
2021-01-21 11:20:48 -0300

    chore: configure movie dependency injection

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,10 +2,18 @@ import { container } from 'tsyringe';
 
 import '@modules/customers/providers';
 
-import CustomersRepository from '@modules/customers/infra/typeorm/repositories/CustomersRepository';
 import ICustomersRepository from '@modules/customers/repositories/ICustomersRepository';
+import CustomersRepository from '@modules/customers/infra/typeorm/repositories/CustomersRepository';
+
+import IMoviesRepository from '@modules/movies/repositories/IMoviesRepository';
+import MoviesRepository from '@modules/movies/infra/typeorm/repositories/MoviesRepository';
 
 container.registerSingleton<ICustomersRepository>(
   'CustomersRepository',
   CustomersRepository,
+);
+
+container.registerSingleton<IMoviesRepository>(
+  'MoviesRepository',
+  MoviesRepository,
 );
