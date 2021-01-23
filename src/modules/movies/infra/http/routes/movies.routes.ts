8e528958dac32472c3fee5019bc915f98961b416
Renato Hideki Osaka <renato.osaka@hotmail.com>
2021-01-23 16:09:51 -0300

    chore: protected movies index route

--- src/modules/movies/infra/http/routes/movies.routes.ts
+++ src/modules/movies/infra/http/routes/movies.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 
 import MoviesController from '@modules/movies/infra/http/controllers/MoviesController';
+import ensureAuthenticated from '@modules/customers/infra/http/middlewares/ensureAuthenticated';
 
 const routes = Router();
 const moviesController = new MoviesController();
 
-routes.get('/', moviesController.index);
+routes.get('/', ensureAuthenticated, moviesController.index);
 
 export default routes;
